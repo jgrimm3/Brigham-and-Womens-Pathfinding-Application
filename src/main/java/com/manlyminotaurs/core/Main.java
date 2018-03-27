aa30cb87b793d1b8f13ba622c1c3ad2c887339b6
ChristianCedron <ccedron@wpi.edu>
2018-03-27 18:42:51 -0400

    Changes to structure made in order to get Gradle to play nice

---
+++ src/main/java/com/manlyminotaurs/core/Main.java
@@ -0,0 +1,23 @@
+package com.manlyminotaurs.core;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../../../CS3733TeamM-Proj/src/com/manlyminotaurs/core/sample.fxml"));
+        primaryStage.setTitle("Loan Calculator");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
