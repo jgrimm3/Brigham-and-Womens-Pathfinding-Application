8fff3788c61f6b9d262a6b15fb21f417f5f3845c
ChristianCedron <ccedron@wpi.edu>
2018-03-28 15:30:41 -0400

    Coppied Getters and Setters for Node Loacation and Transport in order to test things.

--- src/main/java/com/manlyminotaurs/nodes/Node.java
+++ src/main/java/com/manlyminotaurs/nodes/Node.java
@@ -7,4 +7,42 @@ public abstract class Node {
     String shortName;
     String ID;
     String nodeType;
+
+    public Node(String longName, String shortName, String ID, String nodeType, int xcoord, int ycoord, String floor, String building) {
+        this.loc = loc;
+        this.longName = longName;
+        this.shortName = shortName;
+        this.ID = ID;
+        this.nodeType = nodeType;
+        this.loc = new Location(xcoord, ycoord, floor, building);
+    }
+
+    public void setLoc(Location loc) { this.loc = loc; }
+
+    public void setLongName(String longName) { this.longName = longName; }
+
+    public void setShortName(String shortName) { this.shortName = shortName; }
+
+    public void setID(String ID) { this.ID = ID; }
+
+    public void setNodeType(String nodeType) { this.nodeType = nodeType; }
+
+    public Location getLoc() { return loc; }
+
+    public String getID() { return ID; }
+
+    public String getLongName(){return longName; }
+
+    public String getShortName() { return shortName; }
+
+    public String getNodeType() { return nodeType; }
+
+    public int getXCoord() { return loc.xCoord; }
+
+    public int getYCoord() { return loc.yCoord; }
+
+    public String getFloor() { return loc.floor; }
+
+    public String getBuilding() { return loc.building; }
+
 }
