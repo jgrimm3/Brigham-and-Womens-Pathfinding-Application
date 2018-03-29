0d1f2efc2cc08ef42484dab531d0e00a72f01319
Oliver Sanderson <osanderson@wpi.edu>
2018-03-29 12:10:44 -0400

    Two-node A* working in ScoredNode context. Updated constructors in all classes extending Node

--- src/main/java/com/manlyminotaurs/nodes/Transport.java
+++ src/main/java/com/manlyminotaurs/nodes/Transport.java
@@ -1,7 +1,9 @@
 package com.manlyminotaurs.nodes;
 
+import java.util.ArrayList;
+
 public abstract class Transport extends Node {
-    public Transport(String longName, String shortName, String ID, String nodeType, int xcoord, int ycoord, String floor, String building) {
-        super(longName, shortName, ID, nodeType, xcoord, ycoord, floor, building);
+    public Transport(String longName, String shortName, String ID, String nodeType, int xcoord, int ycoord, String floor, String building, ArrayList<Edge> edges) {
+        super(longName, shortName, ID, nodeType, xcoord, ycoord, floor, building, edges);
     }
 }
