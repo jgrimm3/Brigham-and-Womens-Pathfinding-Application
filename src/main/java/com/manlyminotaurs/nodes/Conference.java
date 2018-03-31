ca55885c88c53e8b50ff9d1cdb2c3580f3790bcf
ChristianCedron <ccedron@wpi.edu>
2018-03-31 13:31:50 -0400

    edited Conference

--- src/main/java/com/manlyminotaurs/nodes/Conference.java
+++ src/main/java/com/manlyminotaurs/nodes/Conference.java
@@ -1,7 +1,5 @@
 package com.manlyminotaurs.nodes;
 
-import java.util.IdentityHashMap;
-
 public class Conference extends Room {
     public Conference(String longName, String shortName, String ID, String nodeType, int xcoord, int ycoord, String floor, String building) {
         super(longName, shortName, ID, nodeType, xcoord, ycoord, floor, building);
