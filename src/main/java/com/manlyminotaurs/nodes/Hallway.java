e12d5a3a8d534bcf2cab09fdf4e3594e0c453057
ChristianCedron <ccedron@wpi.edu>
2018-03-31 16:04:26 -0400

    Added atributes and edited constructors to reflect changes to: -Exit.java -Hallway.java -Room.java -Transport.java

--- src/main/java/com/manlyminotaurs/nodes/Hallway.java
+++ src/main/java/com/manlyminotaurs/nodes/Hallway.java
@@ -1,7 +1,16 @@
 package com.manlyminotaurs.nodes;
 
 public class Hallway extends Node {
+    int popularity;
+
     public Hallway(String longName, String shortName, String ID, String nodeType, int xcoord, int ycoord, String floor, String building) {
         super(longName, shortName, ID, nodeType, xcoord, ycoord, floor, building);
+        popularity = 0;
     }
+
+    public Hallway(String longName, String shortName, String ID, String nodeType, int xcoord, int ycoord, String floor, String building, int popularity) {
+        super(longName, shortName, ID, nodeType, xcoord, ycoord, floor, building);
+        this.popularity = popularity;
+    }
+
 }
