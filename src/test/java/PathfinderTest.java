ab3d1d00039d33769adf77c19ea6c524f1725096
Oliver Sanderson <osanderson@wpi.edu>
2018-03-31 16:49:29 -0400

    Trying to run first test

--- src/test/java/PathfinderTest.java
+++ src/test/java/PathfinderTest.java
@@ -1,3 +1,4 @@
+import com.manlyminotaurs.databases.NodesEditor;
 import com.manlyminotaurs.nodes.ScoredNode;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,50 +14,34 @@ import java.util.LinkedList;
 
 public class PathfinderTest {
     private Pathfinder pf;
-    private Restroom restroom1;
-    private Restroom restroom2;
-    private Restroom restroom3;
-    private Restroom restroom4;
-    private Restroom restroom5;
-    private Restroom restroom6;
-    private Edge edge1_2;
-    private Edge edge2_3;
-    private Edge edge2_4;
-    private Edge edge4_5;
-    private Edge edge3_5;
+    private NodesEditor ne;
     private LinkedList<Node> fixture;
+
+    Node retrieveNode(String nodeID) {
+        for (Node node: ne.nodeList) {
+            if (node.getID() == nodeID) {
+                return node;
+            }
+        }
+        return null;
+    }
 
     @Before
     public void createFixtures(){
         pf = new Pathfinder();
-        restroom1 = new Restroom("a", "a", "1", "test", 1, 1, "1", "bldg", new ArrayList<>());
-        restroom2 = new Restroom("a", "a", "2", "test", 2, 2, "1", "bldg", new ArrayList<>());
-        restroom3 = new Restroom("a", "a", "3", "test", 3, 3, "1", "bldg", new ArrayList<>());
-        restroom4 = new Restroom("a", "a", "4", "test", 4, 4, "1", "bldg", new ArrayList<>());
-        restroom5 = new Restroom("a", "a", "5", "test", 5, 5, "1", "bldg", new ArrayList<>());
-        restroom6 = new Restroom("a", "a", "6", "test", 6, 6, "1", "bldg", new ArrayList<>());
-        edge1_2 = new Edge(restroom1, restroom2, "test", 0);
-        edge2_3 = new Edge(restroom2, restroom3, "test", 0);
-        edge2_4 = new Edge(restroom2, restroom4, "test", 0);
-        edge4_5 = new Edge(restroom4, restroom5, "test", 0);
-        edge3_5 = new Edge(restroom3, restroom5, "test", 0);
-        restroom1.getEdges().add(edge1_2);
-        restroom2.getEdges().add(edge1_2);
-        restroom2.getEdges().add(edge2_3);
-        restroom2.getEdges().add(edge2_4);
-        restroom3.getEdges().add(edge2_3);
-        restroom3.getEdges().add(edge3_5);
-        restroom4.getEdges().add(edge2_4);
-        restroom4.getEdges().add(edge4_5);
+        ne = new NodesEditor();
+        ne.retrieveNodes();
+        ne.retrieveEdges();
 
         fixture = new LinkedList<>();
     }
 
     @Test
     public void Find_WhenOnlyOneNode_ShouldReturnNode(){
-        fixture.add(restroom1);
+        Node node1 = retrieveNode("GCONF02001");
+        fixture.add(node1);
 
-        LinkedList<Node> result = pf.find(restroom1, restroom1);
+        LinkedList<Node> result = pf.find(node1, node1);
         assertEquals(fixture, result);
     }
 
