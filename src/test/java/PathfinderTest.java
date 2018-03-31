d7b6e7df3841c8902d7986ddaab7bdfd594c1200
Oliver Sanderson <osanderson@wpi.edu>
2018-03-31 12:43:22 -0400

    Updated gScore to use actual distances instead of number of nodes in trail

--- src/test/java/PathfinderTest.java
+++ src/test/java/PathfinderTest.java
@@ -1,3 +1,4 @@
+import com.manlyminotaurs.nodes.ScoredNode;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
@@ -16,9 +17,13 @@ public class PathfinderTest {
     private Restroom restroom2;
     private Restroom restroom3;
     private Restroom restroom4;
+    private Restroom restroom5;
+    private Restroom restroom6;
     private Edge edge1_2;
     private Edge edge2_3;
     private Edge edge2_4;
+    private Edge edge4_5;
+    private Edge edge3_5;
     private LinkedList<Node> fixture;
 
     @Before
@@ -28,15 +33,21 @@ public class PathfinderTest {
         restroom2 = new Restroom("a", "a", "2", "test", 2, 2, "1", "bldg", new ArrayList<>());
         restroom3 = new Restroom("a", "a", "3", "test", 3, 3, "1", "bldg", new ArrayList<>());
         restroom4 = new Restroom("a", "a", "4", "test", 4, 4, "1", "bldg", new ArrayList<>());
+        restroom5 = new Restroom("a", "a", "5", "test", 5, 5, "1", "bldg", new ArrayList<>());
+        restroom6 = new Restroom("a", "a", "6", "test", 6, 6, "1", "bldg", new ArrayList<>());
         edge1_2 = new Edge(restroom1, restroom2, "test", 0);
         edge2_3 = new Edge(restroom2, restroom3, "test", 0);
         edge2_4 = new Edge(restroom2, restroom4, "test", 0);
+        edge4_5 = new Edge(restroom4, restroom5, "test", 0);
+        edge3_5 = new Edge(restroom3, restroom5, "test", 0);
         restroom1.getEdges().add(edge1_2);
         restroom2.getEdges().add(edge1_2);
         restroom2.getEdges().add(edge2_3);
         restroom2.getEdges().add(edge2_4);
         restroom3.getEdges().add(edge2_3);
+        restroom3.getEdges().add(edge3_5);
         restroom4.getEdges().add(edge2_4);
+        restroom4.getEdges().add(edge4_5);
 
         fixture = new LinkedList<>();
     }
@@ -67,7 +78,6 @@ public class PathfinderTest {
         LinkedList<Node> result = pf.find(restroom1, restroom4);
         assertEquals(fixture, result);
     }
-
 
 }
 
