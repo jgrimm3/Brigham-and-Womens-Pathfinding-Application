0cd86253784fc425c74ef1e832735ee395d89284
jgrimm3 <jgrimm@wpi.edu>
2018-04-11 04:07:35 -0400

    added closimng check to save files when application is closed

--- src/main/java/com/manlyminotaurs/viewControllers/nodeEditorController.java
+++ src/main/java/com/manlyminotaurs/viewControllers/nodeEditorController.java
@@ -422,7 +422,6 @@ public class nodeEditorController {
         }
     }
 
-
     //Combo Box selected update next
     //Add node
     public void addSetBuilding(ActionEvent event) {
@@ -445,7 +444,6 @@ public class nodeEditorController {
     public void addSetType(ActionEvent event) {
         //set type to selected value
         type = cmboType.getValue().toString();
-
     }
 
     //modify node
@@ -468,13 +466,16 @@ public class nodeEditorController {
     public void modSetType(ActionEvent event) {
         //set type to selected value
         type = cmboTypeMod.getValue().toString();
-        cmboNodeMod.setItems(FXCollections.observableArrayList(DataModelI.getInstance().getLongNameByBuildingTypeFloor(cmboBuildingMod.getValue(),cmboTypeMod.getValue(),cmboFloor.getValue())));
+        List<Node> curNode = DataModelI.getInstance().retrieveNodes();
+        List<String> currentN = DataModelI.getInstance().getLongNameByBuildingTypeFloor(cmboBuildingMod.getValue(),cmboTypeMod.getValue(),cmboFloor.getValue());
+        cmboNodeMod.setItems(FXCollections.observableArrayList(currentN));
 
 
     }
 
     public void modSetNode(ActionEvent event) {
         //set type to selected value
+        DataModelI.getInstance().retrieveNodes();
         node = DataModelI.getInstance().getNodeByLongName(cmboNodeMod.getValue().toString());
         txtLongNameMod.setText(node.getLongName());
         txtShortNameMod.setText(node.getShortName());
@@ -504,6 +505,7 @@ public class nodeEditorController {
 
     public void delSetNode(ActionEvent event) {
         //set type to selected value
+        DataModelI.getInstance().retrieveNodes();
         node = DataModelI.getInstance().getNodeByLongName(cmboNodeDel.getValue().toString());
         txtLongNameDel.setText(node.getLongName());
         txtShortNameDel.setText(node.getShortName());
@@ -524,7 +526,10 @@ public class nodeEditorController {
         type = cmboType.getValue().toString();
         //call add node function
         DataModelI.getInstance().addNode(xCoord2D, yCoord2D, floor, building, type, longName, shortName, 1, xCoord3D, yCoord3D);
+        btnAddNode.setText("Node Added!");
         //redraw map
+        drawCircles(cmboFloorAdd.getValue(),"2-D");
+        btnAddNode.setText("Add Node");
     }
 
 
@@ -704,6 +709,7 @@ public void setPathfindAlgorithm(ActionEvent event) {
             pathfloor2DMapLoader(floor);
 
             List<Node> nodeList = new ArrayList<>();
+            DataModelI.getInstance().retrieveNodes();
             nodeList = DataModelI.getInstance().getNodesByFloor(floor);
 
             for(int x=0;x<nodeList.size(); x++) {
