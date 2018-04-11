249781085c8b4930281c9bacb255d2af425e0461
jgrimm3 <jgrimm@wpi.edu>
2018-04-11 01:16:54 -0400

    seting up singleton

--- src/main/java/com/manlyminotaurs/viewControllers/homeController.java
+++ src/main/java/com/manlyminotaurs/viewControllers/homeController.java
@@ -44,6 +44,30 @@ import java.util.ResourceBundle;
 
 public class homeController implements Initializable {
 
+
+    //Nested Private Singleton
+    private static class Singleton {
+        private static Singleton instance = null;
+
+        private Singleton() {
+            PathfindingContext Pf = new PathfindingContext();
+
+        }
+
+        private static class SingletonHolder {
+            private static Singleton MapController = new Singleton();
+
+        }
+
+        public static Singleton getInstance() {
+            return homeController.Singleton.SingletonHolder.MapController;
+        }
+    }
+
+    public void testSingleton() {
+        Singleton.getInstance();
+    }
+
     //-----------------------------------------------------------------------------------------------------------------
     //
     //                                           Create objects
