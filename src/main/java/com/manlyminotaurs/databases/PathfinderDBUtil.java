87180d9bdf6d2cb691dcc7dc0f8d111fb8944832
EdwardJang <jjang@wpi.edu>
2018-04-16 17:39:06 -0400

    Changed makeRequest endtime to give currentTime instead

--- src/main/java/com/manlyminotaurs/databases/PathfinderDBUtil.java
+++ src/main/java/com/manlyminotaurs/databases/PathfinderDBUtil.java
@@ -1,4 +1,24 @@
 package com.manlyminotaurs.databases;
 
 public class PathfinderDBUtil {
+
+    private static int pathFinderIDCounter = 0;
+
+    public static void setPathFinderIDCounter(int logIDCounter) {
+        PathfinderDBUtil.pathFinderIDCounter = pathFinderIDCounter;
+    }
+
+    private String generateLogID(){
+        pathFinderIDCounter++;
+        return Integer.toString(pathFinderIDCounter);
+    }
+
+
+    //retrieve pathfindings
+
+    //Add pathfinding
+
+    //remove pathfinding
+
+    //get pathfinding by IDs
 }
