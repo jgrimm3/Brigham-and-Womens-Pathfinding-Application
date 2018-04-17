c7e42054db51e5fe3aa4ef1948a467d7134e2114
Oliver Sanderson <osanderson@wpi.edu>
2018-04-17 14:19:14 -0400

    Added second test for genExistingRequest

--- src/integration-test/java/DatabaseServiceRequestTest.java
+++ src/integration-test/java/DatabaseServiceRequestTest.java
@@ -4,6 +4,9 @@ import com.manlyminotaurs.nodes.Node;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+
+import java.time.LocalDateTime;
+
 import static com.manlyminotaurs.messaging.RequestType.JanitorialRequest;
 import static org.junit.Assert.assertFalse;
 
@@ -19,6 +22,13 @@ public class DatabaseServiceRequestTest {
         Node testNode = DataModelI.getInstance().getNodeByID("GHALL00201");
 
         assertFalse(rf.genNewRequest(JanitorialRequest, testNode, "test", "4", 1) == null);
+    }
+
+    @Test
+    public void genExistingRequest(){
+        RequestFactory rf = new RequestFactory();
+
+        assertFalse(rf.genExistingRequest("1","Janitorial", 1, false, false, LocalDateTime.now(), LocalDateTime.now(), "GHALL00201", "1", "test") == null);
     }
 
 }
