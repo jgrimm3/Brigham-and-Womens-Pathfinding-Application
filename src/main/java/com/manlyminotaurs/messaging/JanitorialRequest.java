db292d99cc50dfe4aa42b79240bb0518563ac33b
Peter Christakos <newtownfam@gmail.com>
2018-04-25 04:05:15 -0400

    messaging javadocs done

--- src/main/java/com/manlyminotaurs/messaging/JanitorialRequest.java
+++ src/main/java/com/manlyminotaurs/messaging/JanitorialRequest.java
@@ -7,6 +7,11 @@ class JanitorialRequest extends Request{
     JanitorialRequest(String requestID, String requestType, int priority, Boolean isComplete, Boolean adminConfirm, LocalDateTime startTime, LocalDateTime endTime, String nodeID, String messageID, String password) {
         super(requestID, requestType, priority, isComplete, adminConfirm, startTime,endTime, password, nodeID, messageID);
     }
+
+    /**
+     * sets service complated to true
+     * @return
+     */
     @Override
     public boolean service() {
         this.setComplete(true);
